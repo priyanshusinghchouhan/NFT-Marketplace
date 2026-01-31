dc375ea83c56d145cee3f4b4fa3af43f808a9360
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-01-31 19:10:08 +0530

    imported ER721 ABI

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,5 +5,5 @@ import { sepolia } from "wagmi/chains";
 export const SUPPORTED_CHAIN = sepolia;
 
 // ---- Contract ----
-export const NFT_CONTRACT_ADDRESS =
+export const MARKETPLACE_CONTRACT_ADDRESS =
   "0x14098c94258118087820b477bD2B9a38E3cE5371";
