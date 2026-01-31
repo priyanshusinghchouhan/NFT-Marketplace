91746460bba3c36eb2bd1d64e8801ea13874ce01
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-01-31 10:54:54 +0530

    added NFTMarketplace contract address

---
+++ src/constants/constants.ts
@@ -0,0 +1,9 @@
+import { sepolia } from "wagmi/chains";
+
+
+// ---- Chain ----
+export const SUPPORTED_CHAIN = sepolia;
+
+// ---- Contract ----
+export const NFT_CONTRACT_ADDRESS =
+  "0x14098c94258118087820b477bD2B9a38E3cE5371";
