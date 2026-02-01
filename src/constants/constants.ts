aeb85957fe96179e508c40f976af595c2028f614
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-01 10:51:00 +0530

    small fixes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,6 @@
 import { sepolia } from "wagmi/chains";
 
-
-// ---- Chain ----
 export const SUPPORTED_CHAIN = sepolia;
 
-// ---- Contract ----
 export const MARKETPLACE_CONTRACT_ADDRESS =
   "0x14098c94258118087820b477bD2B9a38E3cE5371";
