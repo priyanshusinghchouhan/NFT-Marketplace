235f8a95ca48a7c82636f4a187f190d4e7ec472b
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-06 19:53:50 +0530

    added placeholder images

--- src/mocks/mockListings.ts
+++ src/mocks/mockListings.ts
@@ -45,7 +45,7 @@ export const MOCK_LISTINGS: MarketplaceListing[] = [
   },
   {
     id: 5,
-    image: "/nft-1.jpg",
+    image: "https://i.pinimg.com/736x/9b/c0/4d/9bc04d5e3e74058aa0b5ad629a12db0c.jpg",
     tokenId: "101",
     price: "0.45",
     collection: "Chocotoycute",
@@ -53,7 +53,7 @@ export const MOCK_LISTINGS: MarketplaceListing[] = [
   },
   {
     id: 6,
-    image: "/nft-1.jpg",
+    image: "https://i.pinimg.com/736x/1c/d2/94/1cd294a16e0a84019f4d177aea4016b2.jpg",
     tokenId: "101",
     price: "0.45",
     collection: "Chocotoycute",
@@ -61,7 +61,7 @@ export const MOCK_LISTINGS: MarketplaceListing[] = [
   },
   {
     id: 7,
-    image: "/nft-1.jpg",
+    image: "https://i.pinimg.com/736x/97/f3/a0/97f3a02aacd095dab5ff6e61ea7c0962.jpg",
     tokenId: "101",
     price: "0.45",
     collection: "Chocotoycute",
