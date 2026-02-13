ecbd490d32e30bfe81b9252b14ced6e02402cd2c
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-13 17:55:03 +0530

    feat(Hooks): added Invalidate Queries

--- src/hooks/useCancelListing.ts
+++ src/hooks/useCancelListing.ts
@@ -1,18 +1,26 @@
 import { useWriteContract, useWaitForTransactionReceipt } from "wagmi";
 import { NFTMarketplaceABI } from "@/abi/NFTMarketplace";
 import { MARKETPLACE_CONTRACT_ADDRESS } from "@/constants/constants";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 
 export function useCancelListing() {
+  const queryClient = useQueryClient();
+
   const { data: hash, writeContract, isPending, error } = useWriteContract();
   
   const { isLoading: isConfirming, isSuccess } = useWaitForTransactionReceipt({
     hash,
   });
 
+  const [lastCancelledId, setLastCancelledId] = useState<number | null>(null);
+
   const cancelListing = (listingId: number) => {
     if(listingId < 0) return;
-    
+
+    setLastCancelledId(listingId);
+
     writeContract({
       address: MARKETPLACE_CONTRACT_ADDRESS,
       abi: NFTMarketplaceABI,
@@ -20,6 +28,19 @@ export function useCancelListing() {
       args: [BigInt(listingId)],
     });
   };
+
+  console.log("before useCancel useEffect");
+
+  useEffect(() => {
+    if(isSuccess && lastCancelledId !== null) {
+      console.log("inside useEffect of cancelled listing");
+      setTimeout(() => {
+        queryClient.invalidateQueries({
+          queryKey: ["listings"]
+        })
+      }, 1200);
+    }
+  }, [isSuccess, lastCancelledId, queryClient])
 
   return {
     cancelListing,
--- src/hooks/useUpdateListingPrice.ts
+++ src/hooks/useUpdateListingPrice.ts
@@ -2,16 +2,24 @@ import { useWriteContract, useWaitForTransactionReceipt } from "wagmi";
 import { NFTMarketplaceABI } from "@/abi/NFTMarketplace";
 import { MARKETPLACE_CONTRACT_ADDRESS } from "@/constants/constants";
 import { parseEther } from "viem";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 export function useUpdateListingPrice() {
+  const queryClient = useQueryClient();
+
   const { data: hash, writeContract, isPending, error } = useWriteContract();
   
   const { isLoading: isConfirming, isSuccess } = useWaitForTransactionReceipt({
     hash,
   });
 
+  const [lastUpdatedId, setLastUpdatedId] = useState<number | null>(null);
+
   const updatePrice = (listingId: number, newPrice: string) => {
     if(!newPrice || Number(newPrice) <= 0) return;
+
+    setLastUpdatedId(listingId);
 
     writeContract({
       address: MARKETPLACE_CONTRACT_ADDRESS,
@@ -20,6 +28,19 @@ export function useUpdateListingPrice() {
       args: [BigInt(listingId), parseEther(newPrice)],
     });
   };
+
+  console.log("before useUpdate useEffect");
+
+    useEffect(() => {
+      if(isSuccess && lastUpdatedId !== null) {
+        setTimeout(() => {
+          queryClient.invalidateQueries({
+            queryKey:["listings"]
+          })
+        }, 1200);
+      }
+    }, [isSuccess, lastUpdatedId, queryClient])
+  
 
   return {
     updatePrice,
