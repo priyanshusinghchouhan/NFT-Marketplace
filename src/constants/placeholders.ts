486c6e1f39e8925d9689cf9cc8506ff363a8c82e
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-16 12:06:09 +0530

    feat(constants): add PLACEHOLDER_IMAGES for NFT fallback thumbnails

---
+++ src/constants/placeholders.ts
@@ -0,0 +1,12 @@
+export const PLACEHOLDER_IMAGES = [
+  "https://i.pinimg.com/1200x/e3/75/b5/e375b5bc3d3e2df39d59b7fcad7793bd.jpg",
+  "https://i.pinimg.com/736x/e2/e7/13/e2e71377191de1ef3274cd1664730d27.jpg",
+  "https://i.pinimg.com/736x/fd/b2/f3/fdb2f3b594d52a04de454be42a6ac69d.jpg",
+  "https://i.pinimg.com/1200x/89/42/6b/89426b0cad23f00f762c6768b3292db0.jpg",
+  "https://i.pinimg.com/1200x/51/f0/c1/51f0c1318f77d45ccf4d7a026282adaf.jpg",
+  "https://i.pinimg.com/736x/72/a4/e1/72a4e1f16e86e8f7388d6b95d55bb715.jpg",
+  "https://i.pinimg.com/1200x/21/93/4b/21934b0b34b3b55ab21c683a3677faf1.jpg",
+  "https://i.pinimg.com/736x/97/f3/a0/97f3a02aacd095dab5ff6e61ea7c0962.jpg",
+  "https://i.pinimg.com/736x/1c/d2/94/1cd294a16e0a84019f4d177aea4016b2.jpg",
+  "https://i.pinimg.com/736x/9b/c0/4d/9bc04d5e3e74058aa0b5ad629a12db0c.jpg",
+];
