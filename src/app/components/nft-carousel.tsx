3131489705a332307d0a790e11115c5bb55b2532
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-16 12:14:59 +0530

    refactor(components): import PLACEHOLDER images from constants/ folder

--- src/app/components/nft-carousel.tsx
+++ src/app/components/nft-carousel.tsx
@@ -7,19 +7,7 @@ import { useListings } from "../../hooks/useListings";
 import {FullMarketplaceListing} from "../../types/listing";
 import { NFTDetailModal } from "./NFTDetailModal";
 import { formatEther } from "viem";
-
-const PLACEHOLDER_IMAGES = [
-  "https://i.pinimg.com/1200x/e3/75/b5/e375b5bc3d3e2df39d59b7fcad7793bd.jpg",
-  "https://i.pinimg.com/736x/e2/e7/13/e2e71377191de1ef3274cd1664730d27.jpg",
-  "https://i.pinimg.com/736x/fd/b2/f3/fdb2f3b594d52a04de454be42a6ac69d.jpg",
-  "https://i.pinimg.com/1200x/89/42/6b/89426b0cad23f00f762c6768b3292db0.jpg",
-  "https://i.pinimg.com/1200x/51/f0/c1/51f0c1318f77d45ccf4d7a026282adaf.jpg",
-  "https://i.pinimg.com/736x/72/a4/e1/72a4e1f16e86e8f7388d6b95d55bb715.jpg",
-  "https://i.pinimg.com/1200x/21/93/4b/21934b0b34b3b55ab21c683a3677faf1.jpg",
-  "https://i.pinimg.com/736x/97/f3/a0/97f3a02aacd095dab5ff6e61ea7c0962.jpg",
-  "https://i.pinimg.com/736x/1c/d2/94/1cd294a16e0a84019f4d177aea4016b2.jpg",
-  "https://i.pinimg.com/736x/9b/c0/4d/9bc04d5e3e74058aa0b5ad629a12db0c.jpg",
-];
+import { PLACEHOLDER_IMAGES } from "@/constants/placeholders";
 
 function CarouselCard({listing, }: {
   listing: FullMarketplaceListing;
@@ -125,7 +113,7 @@ export function NFTCarousel() {
               collection: `Marketplace Collection`,
               tokenId: listing.tokenId,
               price: formatEther(BigInt(listing.price)),
-              image: PLACEHOLDER_IMAGES[Number(listing.listingId) % PLACEHOLDER_IMAGES.length],
+              image: PLACEHOLDER_IMAGES[Number(listing.tokenId) % PLACEHOLDER_IMAGES.length],
               seller: listing.seller,
               nftContract: listing.nftContract,
             };
